a68293d3315984b2b248653ff9128403ec50f3e9
myOmikron <git@omikron.dev>
2022-05-19 03:08:04 +0200

    Added dummy status page

--- server/router.go
+++ server/router.go
@@ -3,10 +3,14 @@ package server
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/myOmikron/statuspage/conf"
+	"github.com/myOmikron/statuspage/handler"
 	"gorm.io/gorm"
 )
 
 func defineRoutes(e *echo.Echo, db *gorm.DB, config *conf.Config) {
+	statusWrapper := handler.Wrapper{DB: db}
+
+	e.GET("/", statusWrapper.Status)
 
 	e.Static("/static/", config.Server.StaticPath)
 }
